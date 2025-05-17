fd8fd99d685cc6ff1132bb945d23df35048c09cb
MoonArtyre <43999278+MoonArtyre@users.noreply.github.com>
2025-05-17 14:57:46 +0200

    WebhookVerify

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,14 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const { DISCORD_TOKEN, DISCORD_CLIENT_ID } = process.env;
+const { DISCORD_TOKEN, DISCORD_CLIENT_ID, WEBHOOK_SECRET } = process.env;
 
-if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID) {
+if (!DISCORD_TOKEN || !DISCORD_CLIENT_ID || !WEBHOOK_SECRET) {
     throw new Error("Missing environment variables");
 }
 
 export const config = {
     DISCORD_TOKEN,
     DISCORD_CLIENT_ID,
+    WEBHOOK_SECRET,
 };
--- src/webhookListen.ts
+++ src/webhookListen.ts
@@ -1,13 +1,33 @@
 import express from "express"
 import bodyParser from "body-parser"
+import { Webhooks } from "@octokit/webhooks"
+import { config } from "./config"
+
+const webhooks = new Webhooks({
+    secret: config.WEBHOOK_SECRET,
+})
 
 const app = express()
 const PORT = 80
 //teest
 app.use(bodyParser.json())
 app.post("", (req, res) => {
-    console.log(req.body)
+    console.log(req)
+
+    const signature = req.headers["x-hub-signature-256"]
+
+    if (signature == null || Array.isArray(signature)) {
+        res.status(400).send("Signature error")
+        return
+    }
+
+    if (!webhooks.verify(req.body, signature)) {
+        res.status(401).send("Unauthorized")
+        return
+    }
+
     res.status(200).end()
 })
 
 app.listen(PORT, () => console.log("Server running on ${PORT}"))
+

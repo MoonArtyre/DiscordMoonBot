f193e7c4c9f9c905b04d8f02f7f439186541a6b5
MoonArtyre <43999278+MoonArtyre@users.noreply.github.com>
2025-05-17 15:53:26 +0200

    Webhook signature test 3

--- src/webhookListen.ts
+++ src/webhookListen.ts
@@ -1,12 +1,12 @@
 import express from "express"
-import bodyParser from "body-parser"
+import crypto from "crypto"
 import { config } from "./config"
 
 const app = express()
 const PORT = 80
 //teest
-app.use(bodyParser.json())
-app.post("", async (req, res) => {
+app.use(express.raw({ type: '*/*' }))
+app.post("/GitPost", async (req, res) => {
     const sig = req.headers["x-hub-signature-256"]
 
     if (sig == null || Array.isArray(sig)) {
@@ -14,54 +14,24 @@ app.post("", async (req, res) => {
         return
     }
 
-    console.log(req)
+    if (!await verifySignature(config.WEBHOOK_SECRET, sig, req.body)) {
+        res.status(401).send("Unauthorized Signature")
+        return
+    }
 
-    if (!await verifySignature(config.WEBHOOK_SECRET, sig, req.body))
-
-        res.status(200).end()
+    res.status(200).end()
 })
 
 app.listen(PORT, () => console.log("Server running on ${PORT}"))
 
-const encoder = new TextEncoder();
-
 async function verifySignature(secret: string, signature: string, payload: string): Promise<boolean> {
+    //Webhook sends "hashEncoder=hash" <= (honestly not sure lol), so we split this to use both parts for verification
+    const splitSignature = signature.split("=")
 
-    const algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+    const postHMAC = splitSignature[0]
+    const postSignature = splitSignature[1]
 
-    const keyBytes = encoder.encode(secret);
-    const extractable = false;
-    const key = await crypto.subtle.importKey(
-        "raw",
-        keyBytes,
-        algorithm,
-        extractable,
-        ["sign", "verify"],
-    );
+    const signatureCheck = crypto.createHmac(postHMAC, secret).update(payload).digest("hex")
 
-    const sigBytes = hexToBytes(signature);
-    const dataBytes = encoder.encode(payload);
-    const equal = await crypto.subtle.verify(
-        algorithm.name,
-        key,
-        sigBytes,
-        dataBytes,
-    );
-
-    return equal;
-}
-
-function hexToBytes(hex: string) {
-    const len = hex.length / 2;
-    const bytes = new Uint8Array(len);
-
-    let index = 0;
-    for (let i = 0; i < hex.length; i += 2) {
-        const c = hex.slice(i, i + 2);
-        const b = parseInt(c, 16);
-        bytes[index] = b;
-        index += 1;
-    }
-
-    return bytes;
+    return signatureCheck === postSignature;
 }

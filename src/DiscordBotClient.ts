349ed1d3cf0d4b6b6b43817df32013fde0d52c3d
MoonArtyre <43999278+MoonArtyre@users.noreply.github.com>
2025-05-18 17:32:26 +0200

    Test

--- src/DiscordBotClient.ts
+++ src/DiscordBotClient.ts
@@ -1,11 +1,9 @@
 import { Client } from "discord.js"
 import { UpdateBotStartedStatus } from "./StatusUpdateManager"
-import { setTimeout } from "timers/promises"
 
 export let botClient: Client<true>
 
 export async function OnClientReady(readyClient: Client<true>) {
-    await setTimeout(3000)
 
     //Assign bot client from newly loged in discord session
     botClient = readyClient

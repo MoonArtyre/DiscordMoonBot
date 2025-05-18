6851da61fd866f8bcdae0dff912229ceabf990f3
MoonArtyre <43999278+MoonArtyre@users.noreply.github.com>
2025-05-18 17:55:51 +0200

    Remove git reset

--- src/webhookListen.ts
+++ src/webhookListen.ts
@@ -33,7 +33,6 @@ app.post("/GitPost", async (req, res) => {
     InitializeBotUpdateStatus()
 
     //Start update process
-    await child_process.execSync("git reset --hard > /dev/null 2>&1", { stdio: "ignore" })
     await child_process.execSync("git pull")
     StatusUpdate_DownloadFinished()
 
